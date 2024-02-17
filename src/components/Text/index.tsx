38b4ffa8433e56b9c006e3c3c47bb7c1a0583f74
abyshekhar <abyshekhar@gmail.com>
2024-02-17 07:02:06 +0000

    Code Synced by DhiWise

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const sizeClasses = {
+  txtGilroyBold28: "font-bold font-gilroy",
+  txtGilroyBold18: "font-bold font-gilroy",
+  txtOpenSansRomanSemiBold16: "font-opensans font-semibold",
+  txtMontserratMedium14: "font-medium font-montserrat",
+  txtGilroyBold24: "font-bold font-gilroy",
+  txtGilroySemiBold18Black900: "font-gilroy font-semibold",
+  txtGilroyRegular14Black900: "font-gilroy font-normal",
+  txtGilroyMedium14: "font-gilroy font-medium",
+  txtGilroyMedium15: "font-gilroy font-medium",
+  txtGilroyRegular12: "font-gilroy font-normal",
+  txtGilroySemiBold18: "font-gilroy font-semibold",
+  txtGilroyRegular12Bluegray40001: "font-gilroy font-normal",
+  txtGilroySemiBold16: "font-gilroy font-semibold",
+  txtGilroyMedium16: "font-gilroy font-medium",
+  txtOpenSansRomanSemiBold12: "font-opensans font-semibold",
+  txtGilroyRegular14: "font-gilroy font-normal",
+  txtGilroyMedium16BlueA700: "font-gilroy font-medium",
+  txtGilroySemiBold16Bluegray700: "font-gilroy font-semibold",
+  txtGilroyMedium16Bluegray40001: "font-gilroy font-medium",
+  txtGilroyMedium16Gray90001: "font-gilroy font-medium",
+} as const;
+
+export type TextProps = Partial<{
+  className: string;
+  size: keyof typeof sizeClasses;
+  as: any;
+}> &
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLSpanElement>,
+    HTMLSpanElement
+  >;
+
+const Text: React.FC<React.PropsWithChildren<TextProps>> = ({
+  children,
+  className = "",
+  size,
+  as,
+  ...restProps
+}) => {
+  const Component = as || "p";
+
+  return (
+    <Component
+      className={`text-left ${className} ${size && sizeClasses[size]}`}
+      {...restProps}
+    >
+      {children}
+    </Component>
+  );
+};
+
+export { Text };
